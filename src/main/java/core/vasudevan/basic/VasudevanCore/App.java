81268c63ef60065fed263283e3fce34f55f55529
razzaksr <razzaksr@gmail.com>
2022-08-20 10:20:30 +0530

    project initiated

---
+++ src/main/java/core/vasudevan/basic/VasudevanCore/App.java
@@ -0,0 +1,13 @@
+package core.vasudevan.basic.VasudevanCore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
