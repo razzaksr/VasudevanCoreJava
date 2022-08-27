a1a597df384bee456c0f45ffc20f275b6c006ed9
razzaksr <razzaksr@gmail.com>
2022-08-27 15:08:45 +0530

    while and do while

---
+++ src/main/java/core/vasudevan/basic/VasudevanCore/DoWhileDemonstration.java
@@ -0,0 +1,39 @@
+package core.vasudevan.basic.VasudevanCore;
+
+import java.util.Scanner;
+
+public class DoWhileDemonstration {
+	public static void main(String[] args) {
+		
+		Scanner scan=new Scanner(System.in);
+		
+		int start=0,end=0;
+		System.out.println("Tell us where to start ");
+		start=scan.nextInt();
+		System.out.println("Tell us where to end ");
+		end=scan.nextInt();
+		
+		do {
+			if(start==2||start==3||start==5||start==7||
+					start%3!=0&&start%2!=0&&start%5!=0&&start%7!=0)
+			{
+				System.out.println(start);
+			}
+			start++;
+		}while(start<=end);
+		
+		
+		// exit check
+//		int top=20;
+//		do {
+//			System.out.println(top);
+//			top--;
+//		}while(top>50);
+		
+//		int top=100;
+//		do {
+//			System.out.println(top);
+//			top--;
+//		}while(top>50);
+	}
+}
---
+++ src/main/java/core/vasudevan/basic/VasudevanCore/WhileDemonstration.java
@@ -0,0 +1,101 @@
+package core.vasudevan.basic.VasudevanCore;
+
+import java.util.Scanner;
+
+public class WhileDemonstration {
+	public static void main(String[] args) {
+		
+		Scanner scan=new Scanner(System.in);
+		
+//		System.out.println("Tell us series you wish ");
+//		int series=scan.nextInt();// 5
+//		int steps=series;// init
+//		while(steps<=100) {
+//			System.out.println(steps);
+//			steps+=series;
+//		}
+		
+//		int steps=1;// init
+//		while(steps<=100) {
+//			System.out.println(steps);
+//			steps+=2;
+//		}
+		
+//		int steps=2;// init
+//		while(steps<=100) {
+//			System.out.println(steps);
+//			steps+=2;
+//		}
+		
+//		int steps=1;// init
+//		while(steps<=100) {
+//			System.out.println(steps);
+//			steps++;
+//		}
+		
+//		int steps=100;// init
+//		while(steps>90) {
+//			System.out.println("Core java can help to build microservice "+steps);
+//			System.out.println("tell us iteration ");
+//			steps-=scan.nextInt();
+//		}
+		
+//		int steps=11;// init
+//		while(steps<=20) {
+//			System.out.println("Core java can help to build microservice "+steps);
+//			steps+=3;// iter
+//		}
+		
+//		int steps=100;// init
+//		while(steps>90) {
+//			System.out.println("Core java can help to build microservice "+steps);
+//			steps-=2;// iter
+//		}
+		
+		// infinite loop
+//		int steps=100;// init
+//		while(true) {
+//			System.out.println("Core java can help to build microservice "+steps);
+//			steps--;// iter
+//		}
+		
+		// infinite loop
+//		int steps=100;// init
+//		while(steps>90) {
+//			System.out.println("Core java can help to build microservice "+steps);
+//			//steps--;// iter
+//		}
+		
+//		int steps=100;// init
+//		while(steps>90); {
+//			System.out.println("Core java can help to build microservice "+steps);
+//			steps--;// iter
+//		}
+		
+		// entry check
+//		int steps=100;// init
+//		while(steps<90) {
+//			System.out.println("Core java can help to build microservice "+steps);
+//			steps--;// iter
+//		}
+		
+//		int steps=21;// init
+//		while(steps<=30) {
+//			System.out.println("Core java can help to build microservice "+steps);
+//			steps++;// iter
+//		}
+		
+//		int steps=100;// init
+//		while(steps>90) {
+//			System.out.println("Core java can help to build microservice "+steps);
+//			steps--;// iter
+//		}
+		
+//		int steps=1;// init
+//		while(steps<=10) {
+//			System.out.println("Core java can help to build microservice");
+//			steps++;// iter
+//		}
+		
+	}
+}
