8b10725c677ac36b8ff1384def54b252c3d54ddf
razzaksr <razzaksr@gmail.com>
2022-12-26 11:58:49 +0530

    Modifiers done

---
+++ src/main/java/core/vasudevan/basic/VasudevanCore/oops/DemoMods.java
@@ -0,0 +1,19 @@
+package core.vasudevan.basic.VasudevanCore.oops;
+
+import java.util.Arrays;
+
+public class DemoMods {
+    public static void main(String[] args) {
+        Quiz qz=new Quiz();
+        //System.out.println(qz.arr.length);
+        qz.display();
+        System.out.println(Arrays.toString(qz.getRow(3)));
+        qz.sarcasm("h");
+
+        Builders builders=new Builders();
+
+        builders.greetSome();
+        builders.saySome();
+
+    }
+}
